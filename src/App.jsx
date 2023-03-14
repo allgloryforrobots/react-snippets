ec4c6cae07a0a02e25c77fb7dce10dc14cba5297
FrontendReactJS <allgloryforrobots@gmail.com>
2023-03-14 11:57:17 +0300

    siple_comp

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <div className="post">
+
+        <div className='post__content'>
+          <strong>
+            1. JS
+          </strong>
+          <div>
+            Язык программирования
+          </div>
+        </div>
+
+        <button className="post__btn">
+          Удалить
+        </button>
+      </div>
+
+
+
+    </div>
+  );
+
+}
+
+export default App;
